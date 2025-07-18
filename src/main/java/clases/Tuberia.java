419af7e3026dcd4e06e5f1dd02531f58e69d5737
Alzuu <santi.alzuguren@gmail.com>
2025-07-18 09:13:54 -0300

    Agregé las Clases de Tuberia y ClaveTuberia

---
+++ src/main/java/clases/Tuberia.java
@@ -0,0 +1,73 @@
+package clases;
+
+public class Tuberia {
+    private String nomenclatura;
+    private double caudalMinimo;
+    private double caudalMaximo;
+    private double diametro;
+    private String estado;
+
+    public Tuberia(Ciudad ciudadOrigen, Ciudad ciudadDestino, double caudalMinimo, double caudalMaximo,
+            double diametro) {
+        nomenclatura = crearNomenclatura(ciudadOrigen, ciudadDestino);
+        this.caudalMinimo = caudalMinimo;
+        this.caudalMaximo = caudalMaximo;
+        this.diametro = diametro;
+        this.estado = "ACTIVO";
+    }
+
+    private String crearNomenclatura(Ciudad cOri, Ciudad cDest) {
+        String nom = "";
+        nom = cOri.getNomenclatura();
+        nom += "-" + cDest.getNomenclatura();
+        return nom;
+    }
+
+    public String getNomenclatura() {
+        return this.nomenclatura;
+    }
+
+    public double getCaudalMinimo() {
+        return this.caudalMinimo;
+    }
+
+    public double getCaudalMaximo() {
+        return this.caudalMaximo;
+    }
+
+    public double getDiametro() {
+        return this.diametro;
+    }
+
+    public String getEstado() {
+        return this.estado;
+    }
+
+    public void setCaudalMinimo(double nuevoCMinimo) {
+        this.caudalMinimo = nuevoCMinimo;
+    }
+
+    public void setCaudalMaximo(double nuevoCMaximo) {
+        this.caudalMaximo = nuevoCMaximo;
+    }
+
+    public void setDiametro(double nuevoDiametro) {
+        this.diametro = nuevoDiametro;
+    }
+
+    public void setActivo() {
+        this.estado = "ACTIVO";
+    }
+
+    public void setInactivo() {
+        this.estado = "INACTIVO";
+    }
+
+    public void setEnDis() {
+        this.estado = "EN DISEÑO";
+    }
+
+    public void setEnRep() {
+        this.estado = "EN REPARACION";
+    }
+}
---
+++ src/test/java/testGrafo.java
@@ -0,0 +1,34 @@
+import clases.estructuras.grafos.Grafo;
+
+public class testGrafo {
+    public static void main(String[] args) {
+        Grafo grafo = new Grafo();
+        cargar(grafo);
+        System.out.println(grafo.listarEnProfundidad().toString());
+    }
+
+    public static void cargar(Grafo grafo) {
+        // Vertices.
+        grafo.insertarVertice('H');
+        grafo.insertarVertice('G');
+        grafo.insertarVertice('E');
+        grafo.insertarVertice('F');
+        grafo.insertarVertice('D');
+        grafo.insertarVertice('C');
+        grafo.insertarVertice('B');
+        grafo.insertarVertice('A');
+
+        // Arcos.
+        grafo.insertarArco('A', 'B', 1);
+        grafo.insertarArco('B', 'F', 6);
+        grafo.insertarArco('B', 'D', 2);
+        grafo.insertarArco('D', 'G', 3);
+        grafo.insertarArco('D', 'E', 4);
+        grafo.insertarArco('G', 'E', 5);
+        grafo.insertarArco('C', 'A', 7);
+        grafo.insertarArco('C', 'F', 8);
+        grafo.insertarArco('C', 'H', 9);
+
+}
+
+}
