797f7a7b1d42881e72663aea983682b0fddb217c
Alzuu <santi.alzuguren@gmail.com>
2025-07-31 16:39:17 -0300

    Cambios de prueba

--- src/test/java/testGrafo.java
+++ src/test/java/testGrafo.java
@@ -62,6 +62,7 @@ public class TestGrafo {
         System.out.println("Insertar otro arco en un lugar donde ya existe un arco: ");
         System.out.println("Insertar un arco con etiqueta nula: ");
         System.out.println("Insertar un ciclo:");
+        System.out.println("Hola Nico, cambié el main");     
     }
 
     public static void cargar(Grafo grafo) {
